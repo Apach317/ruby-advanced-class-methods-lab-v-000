2fa3651d26d37593c463a7bfb2ae75c5bdd38125
Ron Lynch <lynch.ronald1@gmail.com>
2019-01-13 19:40:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,8 +47,8 @@ class Song
 
   def self.new_from_filename(filename)
     new_file = filename.split(" - ")
-    artist = new_file[0].chomp(".mp3")
-    song = new_file[1]
+    artist = new_file[0]
+    song = new_file[1].chomp(".mp3")
     new_song = self.new
     new_song.artist_name = artist
     new_song.name = song
