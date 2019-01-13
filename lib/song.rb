69efecb65d58d55301a51e65def4b3e9e6f57459
Ron Lynch <lynch.ronald1@gmail.com>
2019-01-13 19:39:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,7 +45,7 @@ class Song
     @@all.sort_by {|song| song.name}
   end
 
-  def self.new_from_filename(name)
+  def self.new_from_filename(filename)
     new_file = filename.split(" - ")
     artist = new_file[0]
     song = new_file[1]
